cab06cebaebda74454deb4aed87bb8c488c7ed07
alexander.nikiforov <nikiforov.san.sanich@gmail.com>
2023-09-30 23:39:50 +0400

    03. ClientRows

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,7 @@
-import { gql, useQuery } from '@apollo/client';
+import {gql, useQuery} from '@apollo/client';
+import ClientRow from "./ClientRow";
 
-// const GET_CLIENTS = gql`
-const GET_CLIENTS = `
+const GET_CLIENTS = gql`
     query getClients {
         clients {
             id, name, email, phone
@@ -10,10 +10,28 @@ const GET_CLIENTS = `
 `
 
 export default function Clients() {
-    const { loading, error, data } = useQuery(GET_CLIENTS)
+    const {loading, error, data} = useQuery(GET_CLIENTS)
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Something went wrong</p>
     return (
-        <div>
-            <h1>Clients</h1>
-        </div>
+        <>
+            {!loading && !error &&
+                (<table className='table table-hover mt-3'>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {data.clients.map(client => (
+                            <ClientRow key={client.id} client={client}/>
+                        ))}
+                    </tbody>
+                </table>)}
+        </>
+
     )
 }
